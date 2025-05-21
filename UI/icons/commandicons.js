e1c9a4f6486fa5afa17c761e2b065d3692d89b46
ArnabBiswas18 <biswasarnab2005@gmail.com>
2025-05-22 01:49:15 +0530

    errors fixes

--- UI/icons/commandicons.js
+++ UI/icons/commandicons.js
@@ -8,7 +8,7 @@ const cmdIcons = {
     msgIcon : 'https://cdn.discordapp.com/emojis/915167284648116244.png',
     rippleIcon : 'https://cdn.discordapp.com/emojis/903301195307843595.gif',
     levelUpIcon : 'https://cdn.discordapp.com/emojis/908223980438163476.gif',
-    SSRRIcon : 'https://cdn.discordapp.com/emojis/1334648756649590805.png'
+    SSRRIcon : 'https://cdn.discordapp.com/attachments/1337003098236911647/1374841589658161375/EvilGrin.png?ex=682f8482&is=682e3302&hm=7a507040dcc670d441c7a97922f7483fcfc8758aceea25b87445336cffc9e99f&'
 };
 
 module.exports = cmdIcons;
